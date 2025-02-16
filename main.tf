03164b948c4f371e91808b3b02323df73f3fed7c
jack-liu601 <jcl-liu@hotmail.com>
2025-02-16 18:26:18 +1100

    update on rds syntax

--- main.tf
+++ main.tf
@@ -6,15 +6,17 @@ resource "aws_db_instance" "prod_rds_instance" {
   max_allocated_storage               = 1000  # Allows auto-scaling
   iops                                = 13000 # Provisioned IOPS
   engine                              = "postgres"
-  engine_version                      = "13.7"
+  engine_version                      = "17.3"
   port                                = "5432"
-  parameter_group_name                = aws_db_parameter_group.prod-rds-param-group.name
-  db_subnet_group_name                = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids              = [aws_security_group.rds_security_group.id]
+  parameter_group_name                = aws_db_parameter_group.prod_rds_param_group.name
+  db_subnet_group_name                = aws_db_subnet_group.prod_rds_subnet_group.name
+  vpc_security_group_ids              = [aws_security_group.prod_rds_sg.id]
   publicly_accessible                 = false
   storage_type                        = "gp3" # Select gp3 over io1 / io2 due to cost as there was no requirement for max iops but only min. 13000 IOPS
   multi_az                            = true
   iam_database_authentication_enabled = true
+  skip_final_snapshot                 = true
+  username                            = "dbsuperuser"
   manage_master_user_password         = true
   tags = {
     Name = "prod-devops-rds-instance"
@@ -24,7 +26,7 @@ resource "aws_db_instance" "prod_rds_instance" {
 # RDS Parameter Group
 resource "aws_db_parameter_group" "prod_rds_param_group" {
   name        = "prod-devops-rds-param-group"
-  family      = "postgres13"
+  family      = "postgres17"
   description = "Production RDS Custom parameter group"
 
   parameter {
@@ -44,7 +46,7 @@ resource "aws_db_parameter_group" "prod_rds_param_group" {
 
 # RDS Subnet Group
 resource "aws_db_subnet_group" "prod_rds_subnet_group" {
-  name       = "rds-production-subnet-group"
+  name       = "prod-devops-rds-subnet-group"
   subnet_ids = var.rds_subnet_ids
 
   tags = {
@@ -59,7 +61,7 @@ resource "aws_security_group" "prod_rds_sg" {
   vpc_id      = var.rds_vpc_id
 
   ingress {
-    description = "Allow EKS App CIDR: ${eks_vpc_cidr} connection "
+    description = "Allow EKS App CIDR: ${var.eks_vpc_cidr} connection "
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
